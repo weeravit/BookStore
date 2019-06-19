443bf6228a901d0b214be63f34f412c4d187bc77
Weeravit Sirita <weeravit.sirita@gmail.com>
2019-06-20 01:25:52 +0700

    disable spring-security auto-config

--- src/main/java/com/junebookstore/JuneBookstoreApplication.java
+++ src/main/java/com/junebookstore/JuneBookstoreApplication.java
@@ -2,12 +2,13 @@ package com.junebookstore;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = {SecurityAutoConfiguration.class})
 public class JuneBookstoreApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(JuneBookstoreApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(JuneBookstoreApplication.class, args);
+    }
 
 }
