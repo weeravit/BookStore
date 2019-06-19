d3d9a9364fe11630ea51351a60397ba9d9eab6f1
Weeravit Sirita <weeravit.sirita@gmail.com>
2019-06-19 20:43:13 +0700

    init project

---
+++ src/main/java/com/junebookstore/JuneBookstoreApplication.java
@@ -0,0 +1,13 @@
+package com.junebookstore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JuneBookstoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JuneBookstoreApplication.class, args);
+	}
+
+}
