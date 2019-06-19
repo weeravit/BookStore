ce7b9845bf9c2b1d5288050bd145f7fff2eb3850
Weeravit Sirita <weeravit.sirita@gmail.com>
2019-06-19 23:51:52 +0700

    fix book entity

--- src/main/java/com/junebookstore/entity/BookEntity.java
+++ src/main/java/com/junebookstore/entity/BookEntity.java
@@ -1,13 +1,20 @@
 package com.junebookstore.entity;
 
+import javax.persistence.Entity;
+import javax.persistence.Id;
 import java.util.Objects;
 
+@Entity
 public class BookEntity {
+    @Id
     private int id;
     private double price;
     private String bookName;
     private String authorName;
     private boolean isRecommended;
+
+    public BookEntity() {
+    }
 
     public BookEntity(int id, double price, String bookName, String authorName, boolean isRecommended) {
         this.id = id;
