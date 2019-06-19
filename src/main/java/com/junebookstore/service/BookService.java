938869c65f7d45dfebb9f1658522604449efd81e
Weeravit Sirita <weeravit.sirita@gmail.com>
2019-06-19 22:58:56 +0700

    add logic get books in BookService

--- src/main/java/com/junebookstore/service/BookService.java
+++ src/main/java/com/junebookstore/service/BookService.java
@@ -5,7 +5,9 @@ import com.junebookstore.repository.BookBaseRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
 
 @Service
 public class BookService {
@@ -13,6 +15,20 @@ public class BookService {
     BookBaseRepository repository;
 
     public List<BookEntity> getBooks() {
-        return repository.getBooks();
+        List<BookEntity> books = repository.getBooks();
+        List<BookEntity> recommendation = repository.getBooksRecommendation()
+                .stream()
+                .peek(recommend -> recommend.setRecommended(true))
+                .collect(Collectors.toList());
+
+        List<BookEntity> booksWithoutRecommend = books.stream()
+                .filter(book -> recommendation.stream().noneMatch(recommend -> recommend.equals(book)))
+                .collect(Collectors.toList());
+
+        ArrayList<BookEntity> normalWithRecommend = new ArrayList<>();
+        normalWithRecommend.addAll(recommendation);
+        normalWithRecommend.addAll(booksWithoutRecommend);
+
+        return normalWithRecommend;
     }
 }
