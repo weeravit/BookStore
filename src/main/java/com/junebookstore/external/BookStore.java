c51e719f35925652140ce5c35cf20fe9fb9a6be1
Weeravit Sirita <weeravit.sirita@gmail.com>
2019-06-19 23:50:41 +0700

    add external api class

---
+++ src/main/java/com/junebookstore/external/BookStore.java
@@ -0,0 +1,53 @@
+package com.junebookstore.external;
+
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.junebookstore.entity.BookEntity;
+import com.junebookstore.model.Book;
+import org.springframework.web.client.RestTemplate;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+public class BookStore {
+    private final String BASE_URL = "https://scb-test-book-publisher.herokuapp.com";
+
+    private ObjectMapper objectMapper = new ObjectMapper();
+    private RestTemplate restTemplate = new RestTemplate();
+
+    private static BookStore ourInstance = new BookStore();
+
+    public static BookStore getInstance() {
+        return ourInstance;
+    }
+
+    private BookStore() {
+    }
+
+    public List<BookEntity> getBooks() {
+        String url = String.format("%s/books", BASE_URL);
+        String json = restTemplate.getForObject(url, String.class);
+
+        try {
+            return objectMapper.readValue(json, new TypeReference<List<Book>>() {
+            });
+        } catch (IOException e) {
+            e.printStackTrace();
+            return new ArrayList<>();
+        }
+    }
+
+    public List<BookEntity> getBooksRecommendation() {
+        String url = String.format("%s/books/recommendation", BASE_URL);
+        String json = restTemplate.getForObject(url, String.class);
+
+        try {
+            return objectMapper.readValue(json, new TypeReference<List<Book>>() {
+            });
+        } catch (IOException e) {
+            e.printStackTrace();
+            return new ArrayList<>();
+        }
+    }
+}
--- src/main/java/com/junebookstore/service/BookService.java
+++ src/main/java/com/junebookstore/service/BookService.java
@@ -1,31 +1,19 @@
 package com.junebookstore.service;
 
 import com.junebookstore.entity.BookEntity;
-import com.junebookstore.repository.BookBaseRepository;
+import com.junebookstore.external.BookStore;
+import com.junebookstore.repository.BookRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import java.util.ArrayList;
 import java.util.List;
-import java.util.stream.Collectors;
 
 @Service
 public class BookService {
     @Autowired
-    BookBaseRepository repository;
+    BookRepository repository;
 
     public List<BookEntity> getBooks() {
-        List<BookEntity> books = repository.getBooks();
-        List<BookEntity> recommendation = repository.getBooksRecommendation();
-
-        List<BookEntity> booksWithoutRecommend = books.stream()
-                .filter(book -> recommendation.stream().noneMatch(recommend -> recommend.equals(book)))
-                .collect(Collectors.toList());
-
-        ArrayList<BookEntity> normalWithRecommend = new ArrayList<>();
-        normalWithRecommend.addAll(recommendation);
-        normalWithRecommend.addAll(booksWithoutRecommend);
-
-        return normalWithRecommend;
+        return BookStore.getInstance().getBooksRecommendation();
     }
 }
