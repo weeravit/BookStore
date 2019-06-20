a0c0438919db4f9d3e80bfbd46bd24bf4cf854b8
Weeravit Sirita <weeravit.sirita@gmail.com>
2019-06-20 13:46:10 +0700

    add secure helper

---
+++ src/main/java/com/junebookstore/helper/SecureHelper.java
@@ -0,0 +1,18 @@
+package com.junebookstore.helper;
+
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Component;
+
+@Component
+public class SecureHelper {
+    private PasswordEncoder passwordEncoder = new BCryptPasswordEncoder();
+
+    public String encodePassword(String plain) {
+        return passwordEncoder.encode(plain);
+    }
+
+    public PasswordEncoder getPasswordEncoder() {
+        return passwordEncoder;
+    }
+}
