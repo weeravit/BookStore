5c28aa363930d291ac656ef96e151990d529fdb6
Weeravit Sirita <weeravit.sirita@gmail.com>
2019-06-20 13:06:27 +0700

    fix return type user repository

--- src/main/java/com/junebookstore/repository/UserRepository.java
+++ src/main/java/com/junebookstore/repository/UserRepository.java
@@ -4,4 +4,5 @@ import com.junebookstore.entity.UserEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface UserRepository extends JpaRepository<UserEntity, Integer> {
+    UserEntity findByUsername(String username);
 }
