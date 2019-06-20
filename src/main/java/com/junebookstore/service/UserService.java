76d90a7b2a5129df80f9f06df53c6c6ec6067b64
Weeravit Sirita <weeravit.sirita@gmail.com>
2019-06-20 13:07:43 +0700

    user service implement security login

--- src/main/java/com/junebookstore/service/UserService.java
+++ src/main/java/com/junebookstore/service/UserService.java
@@ -1,11 +1,46 @@
 package com.junebookstore.service;
 
+import com.junebookstore.entity.UserEntity;
+import com.junebookstore.model.Register;
+import com.junebookstore.model.UserPrincipal;
 import com.junebookstore.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 @Service
-public class UserService {
+public class UserService implements UserDetailsService {
     @Autowired
     UserRepository repository;
+
+    public int register(Register data) {
+        UserEntity entity = new UserEntity(
+                data.getUsername(),
+                passwordEncoder().encode(data.getPassword()),
+                data.getDateOfBirth()
+        );
+
+        UserEntity saved = repository.save(entity);
+
+        return saved.getId();
+    }
+
+    private PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        UserEntity entity = repository.findByUsername(username);
+
+        if (entity == null) {
+            throw new UsernameNotFoundException(username);
+        }
+
+        return new UserPrincipal(entity);
+    }
 }
