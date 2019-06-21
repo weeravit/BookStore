5c35b562d0f2f1576304f4c5c29220d3b3fc1947
Weeravit Sirita <weeravit.sirita@gmail.com>
2019-06-21 14:48:50 +0700

    add load books into db at application ready

--- src/main/java/com/junebookstore/JuneBookstoreApplication.java
+++ src/main/java/com/junebookstore/JuneBookstoreApplication.java
@@ -1,11 +1,23 @@
 package com.junebookstore;
 
+import com.junebookstore.service.BookService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.event.ApplicationReadyEvent;
+import org.springframework.context.event.EventListener;
 
 @SpringBootApplication()
 public class JuneBookstoreApplication {
+    @Autowired
+    BookService bookService;
+
     public static void main(String[] args) {
         SpringApplication.run(JuneBookstoreApplication.class, args);
+    }
+
+    @EventListener(ApplicationReadyEvent.class)
+    public void initLoader() {
+        bookService.cacheBooks();
     }
 }
