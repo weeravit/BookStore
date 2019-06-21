91094c764a2fb5fc797215882ca92a5c8de748b5
Weeravit Sirita <weeravit.sirita@gmail.com>
2019-06-21 14:23:36 +0700

    add detail document swagger of user controller

--- src/main/java/com/junebookstore/controller/UserController.java
+++ src/main/java/com/junebookstore/controller/UserController.java
@@ -6,12 +6,15 @@ import com.junebookstore.model.Register;
 import com.junebookstore.model.UserInformation;
 import com.junebookstore.service.OrderService;
 import com.junebookstore.service.UserService;
+import io.swagger.annotations.*;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import javax.servlet.http.HttpServletResponse;
 import java.security.Principal;
 
+@Api("User")
 @RestController
 public class UserController {
     @Autowired
@@ -19,26 +22,52 @@ public class UserController {
     @Autowired
     OrderService orderService;
 
+    @ApiOperation(value = "Create user account")
+    @ApiResponses(value = {
+            @ApiResponse(code = HttpServletResponse.SC_OK, message = "create account success")
+    })
     @PostMapping("/users")
-    public ResponseEntity<?> createUserAccount(@RequestBody Register request) {
+    public ResponseEntity<?> createUserAccount(
+            @ApiParam(value = "user information", required = true)
+            @RequestBody Register request
+    ) {
         userService.register(request);
         return ResponseEntity.ok().build();
     }
 
+    @ApiOperation(value = "Get user information")
+    @ApiResponses(value = {
+            @ApiResponse(code = HttpServletResponse.SC_OK, message = "create account success", response = UserInformation.class),
+            @ApiResponse(code = HttpServletResponse.SC_UNAUTHORIZED, message = "Login required")
+    })
     @GetMapping("/users")
     public ResponseEntity<?> getUserInformation(Principal principal) {
         UserInformation userInformation = userService.getInformation(principal.getName());
         return ResponseEntity.ok(userInformation);
     }
 
+    @ApiOperation(value = "Delete user activity")
+    @ApiResponses(value = {
+            @ApiResponse(code = HttpServletResponse.SC_OK, message = "delete user activity success"),
+            @ApiResponse(code = HttpServletResponse.SC_UNAUTHORIZED, message = "Login required")
+    })
     @DeleteMapping("/users")
     public ResponseEntity<?> deleteUserActivity(Principal principal) {
         orderService.deleteOrderHistory(principal.getName());
         return ResponseEntity.ok().build();
     }
 
+    @ApiOperation(value = "Order books")
+    @ApiResponses(value = {
+            @ApiResponse(code = HttpServletResponse.SC_OK, message = "order books success", response = OrderPrice.class),
+            @ApiResponse(code = HttpServletResponse.SC_UNAUTHORIZED, message = "Login required")
+    })
     @PostMapping("/users/orders")
-    public ResponseEntity<?> orderBooks(Principal principal, @RequestBody OrderBooks request) {
+    public ResponseEntity<?> orderBooks(
+            Principal principal,
+            @ApiParam(value = "order information", required = true)
+            @RequestBody OrderBooks request
+    ) {
         OrderPrice orderPrice = orderService.orderBooks(principal.getName(), request);
         return ResponseEntity.ok(orderPrice);
     }
