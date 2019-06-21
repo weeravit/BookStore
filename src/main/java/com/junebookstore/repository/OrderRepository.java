da51ee4a4ceed9aede5694362a6c22b40022dbdb
Weeravit Sirita <weeravit.sirita@gmail.com>
2019-06-21 14:59:36 +0700

    add repository annotation

--- src/main/java/com/junebookstore/repository/OrderRepository.java
+++ src/main/java/com/junebookstore/repository/OrderRepository.java
@@ -3,9 +3,11 @@ package com.junebookstore.repository;
 import com.junebookstore.entity.OrderEntity;
 import com.junebookstore.entity.UserEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
+@Repository
 public interface OrderRepository extends JpaRepository<OrderEntity, Integer> {
     List<OrderEntity> findByUser(UserEntity user);
 }
