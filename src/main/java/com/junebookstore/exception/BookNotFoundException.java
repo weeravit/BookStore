9df5576f08ec972a267bba4fb2923621ece02bda
Weeravit Sirita <weeravit.sirita@gmail.com>
2019-06-21 16:45:28 +0700

    remove files in old package

--- src/main/java/com/junebookstore/exception/BookNotFoundException.java
+++
@@ -1,7 +0,0 @@
-package com.junebookstore.exception;
-
-public class BookNotFoundException extends RuntimeException {
-    public BookNotFoundException(String message) {
-        super(message);
-    }
-}
